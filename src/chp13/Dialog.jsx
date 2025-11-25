92feb2c8fc1726ffb33fcb1f54cac5d5d09d679b
hanmose <nightsound5124@gmail.com>
2025-11-25 16:06:37 +0900

    Chp13

--- src/chp13/Dialog.jsx
+++ src/chp13/Dialog.jsx
@@ -10,13 +10,23 @@ function Dialog(props) {
     );
 }
 
-function WelcomeDialog() {
+function WelcomeDialogs() {
     return (
-        <Dialog
-            title="어서 오세요"
-            message="우리 사이트에 방문하신 것을 환영합니다!"
-        />
+        <div>
+            <Dialog
+                title="어서 오세요"
+                message="우리 사이트에 방문하신 것을 환영합니다!"
+            />
+            <Dialog
+                title="공지사항"
+                message="오늘은 서버 점검이 예정되어 있습니다."
+            />
+            <Dialog
+                title="이벤트"
+                message="한정판 레드카펫 입장 이벤트에 참여하세요!"
+            />
+        </div>
     );
 }
 
-export default WelcomeDialog;
+export default WelcomeDialogs;
--- src/chp13/FancyBorder.jsx
+++ src/chp13/FancyBorder.jsx
@@ -4,7 +4,6 @@ import "./Castle.css";
 function FancyBorder(props) {
     return (
         <div className={'FancyBorder FancyBorder-' + props.color}>
-            {/* CSS 애니메이션용 요소 */}
             <div className="smoke"></div>
             <div className="light-glow"></div>
             {props.children}
